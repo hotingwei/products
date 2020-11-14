431d71cad8df58803de5248ae257ac51497ad682
hotingwei <anitaho577@gmail.com>
2020-11-14 10:38:27 +0100

    add header

--- products.py
+++ products.py
@@ -4,11 +4,17 @@ while True:
 	if name == 'q': #q = quit
 	    break
 	price = input('Pleas enter the price of the product:')
+	price = int(price)
 	p = [name, price]
 	products.append(p)
 print(products)
 
 #products[0][0] # 第一個0是大清單的第0格，第二個0是小清單的第0格
 
-for product in products:
-	print('The price of', product[0], 'is', product[1])
+for p in products:
+	print('The price of', p[0], 'is', p[1])
+
+with open ('products.csv', 'w', encoding='utf-8') as f: 
+	f.write('product,price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') #真正寫入 #因為先前把p[1]轉為數字，現在因為加號關係需要轉換成字串
