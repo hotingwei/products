1df5f558600462d754cec60565d971d79cee8d4b
hotingwei <anitaho577@gmail.com>
2020-11-14 09:48:12 +0100

    add products.py

---
+++ products.py
@@ -0,0 +1,11 @@
+products = []
+while True:
+	name = input('Please enter the product:')
+	if name == 'q': #q = quit
+	    break
+	price = input('Pleas enter the price of the product:')
+	p = [name, price]
+	products.append(p)
+print(products)
+
+#products[0][0] # 第一個0是大清單的第0格，第二個0是小清單的第0格
